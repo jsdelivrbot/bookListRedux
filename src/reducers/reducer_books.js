9e982c007aae7ef07a1568385a084143a96146ed
Joni Honkanen <joge.honkanen@gmail.com>
2017-05-22 17:12:07 +0300

    bookList harjoitus Eka

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'testi kirja 1' },
+        { title: 'testi kirja 2' },
+        { title: 'testi kirja 3' },
+        { title: 'testi kirja 4' }
+    ];
+}
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+
+export default (
+    <Route path="/" component={App}>
+
+    </Route>
+);
+
+/*
+        <IndexRoute component={} />
+        <Route path="Posts/new" component={} />
+ */
